1ddf1a183b358bd978eb8bdceefd90b7ef9f7a38
aditya-tezsure <aditya@tezsure.com>
2020-06-22 21:18:15 +0530

    first commit

---
+++ precache-manifest.162011bc423ae6c348e0899c55c0d4f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a7a07099bb1a811039d432b9212845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84b74223a6225c5512c",
+    "url": "/static/css/main.5351ac96.chunk.css"
+  },
+  {
+    "revision": "b42df16acc8b230d532d",
+    "url": "/static/js/2.0626b05d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0626b05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84b74223a6225c5512c",
+    "url": "/static/js/main.feb60ea5.chunk.js"
+  },
+  {
+    "revision": "717f7f79fd215dc5d076",
+    "url": "/static/js/runtime-main.f8df27c6.js"
+  },
+  {
+    "revision": "a0d6290547725332706ed5a352acdbb0",
+    "url": "/static/media/Gilroy-ExtraBold.a0d62905.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/Gilroy-Light.c62aded7.otf"
+  }
+]);
